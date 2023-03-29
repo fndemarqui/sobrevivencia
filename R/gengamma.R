56dc738216523a8e06d179c6b7ce25afcd2e051d
fndemarqui <fndemarqui@gmail.com>
2023-03-29 08:10:29 -0300

    gengamma dist

---
+++ R/gengamma.R
@@ -0,0 +1,55 @@
+
+#' @export
+qggamma <- function(p, alpha, gamma, kappa){
+  q <- qgamma(p, shape = alpha/kappa, scale = 1)
+  x <- gamma*(q^(1/kappa))
+  return(x)
+}
+
+#' @export
+rggamma <- function(n, alpha, gamma, kappa){
+  u <- runif(n)
+  x <- qggamma(u, alpha, gamma, kappa)
+  return(x)
+}
+
+#' @export
+dggamma <- function(x, alpha, gamma, kappa, log = FALSE){
+  lpdf <- log(kappa) - alpha*log(gamma) - lgamma(alpha/kappa) + (alpha-1)*log(x) - (x/gamma)^kappa
+  if(isTRUE(log)){
+    return(lpdf)
+  }else{
+    return(exp(lpdf))
+  }
+}
+
+#' @export
+pggamma <- function(q, alpha, gamma, kappa, log = FALSE, lower.tail = TRUE){
+  x <- (q/gamma)^kappa
+  p <- pgamma(x, shape = alpha/kappa, scale = 1)
+  if(isTRUE(log)){
+    if(isTRUE(lower.tail)){
+      return(log(p))
+    }else{
+      return(log(1-p))
+    }
+  }else{
+    if(isTRUE(lower.tail)){
+      return(p)
+    }else{
+      return(1-p)
+    }
+  }
+}
+
+#' @export
+hggamma <- function(x, alpha, gamma, kappa, log = FALSE){
+  lpdf <- dggamma(x, alpha, gamma, kappa, log = TRUE)
+  lccdf  <- pggamma(x, alpha, gamma, kappa, log = TRUE, lower.tail = FALSE)
+  lhf <- lpdf - lccdf
+  if(isTRUE(log)){
+    return(lhf)
+  }else{
+    return(exp(lhf))
+  }
+}
---
+++ R/ggresiduals.R
@@ -0,0 +1,46 @@
+
+# criando a função generica ggresiduals
+
+#' @export
+ggresiduals <- function(object, ...) UseMethod("ggresiduals")
+
+
+#' @export
+ggresiduals.survreg <- function(object, type = c("I", "II")){
+  type <- match.arg(type)
+  dist <- object$dist
+  mf <- model.frame(object)
+  time <- model.extract(mf, "response")[,1]
+  status <- model.extract(mf, "response")[,2]
+  epsilon <- with(object, (log(time)-linear.predictors)/scale)
+  nu <- exp(epsilon)
+  ekm <- survfit(Surv(nu, status)~1)
+  surv <- switch(dist,
+                 exponential = pexp(ekm$time, lower=FALSE,),
+                 weibull = pexp(ekm$time, lower=FALSE),
+                 lognormal = plnorm(ekm$time, lower=FALSE),
+                 loglogistic = 1/(1+ekm$time)
+  )
+
+
+  df <- data.frame(time = ekm$time, surv1 = ekm$surv, surv2 = surv)
+  if(type == "I"){
+    p <- ggplot(df, aes(x = time)) +
+      geom_step(aes(y = surv1)) +
+      geom_line(aes(y = surv2), color = "blue") +
+      ylim(0, 1) +
+      xlab("exp(standardized residuals)") +
+      ylab("Survival") +
+      ggtitle(object$dist)
+    p
+  }else{
+    n <- nrow(df)
+    p <- ggplot(df, aes(x = surv1, y = surv2)) +
+      geom_point() +
+      geom_line(aes(x = seq(0, 1, length.out = n), y = seq(0, 1, length.out = n)), color = "blue") +
+      xlab("Kaplan-Meyer") +
+      ylab(object$dist)
+    p
+  }
+
+}
