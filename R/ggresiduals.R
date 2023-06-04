54e4d4682f760f5db405169ed18570b5c086c80d
fndemarqui <fndemarqui@gmail.com>
2023-06-04 11:01:34 -0300

    adding ggresiduals.coxph

--- R/ggresiduals.R
+++ R/ggresiduals.R
@@ -13,10 +13,10 @@
 ggresiduals <- function(object, ...) UseMethod("ggresiduals")
 
 
-#' ggresiduals method for survstan models
+#' ggresiduals method for survreg models
 #' @aliases ggresiduals.survreg
 #' @export
-#' @param object a fitted model object of the class survstan.
+#' @param object a fitted model object of the class survreg.
 #' @details This function produces residuals plots of Cox-Snell residuals, martingale residuals and deviance residuals.
 #' @param type type of residuals used in the plot: coxsnell (default), martingale and deviance.
 #' @param ... further arguments passed to or from other methods.
@@ -70,6 +70,94 @@ ggresiduals.survreg <- function(object, type = c("coxsnell", "martingale", "devi
       labels <- names(mf)[-1]
       tb <- mf %>%
         select(labels)
+      labels <- names(tb)
+      q <- ncol(tb)
+
+      plots <- list()
+      for(j in 1:q){
+        df <- data.frame(
+          x = tb[,j],
+          martingale = martingale
+        )
+        if(is.factor(df$x)){
+          plots[[j]] <- ggplot(data = df, aes(x = x, y = martingale)) +
+            geom_jitter(position=position_jitter(0.1))
+          xlab(labels[j])
+        }else{
+          plots[[j]] <- ggplot(data = df, aes(x = x, y = martingale)) +
+            geom_point() +
+            geom_smooth(se = FALSE) +
+            xlab(labels[j])
+        }
+      }
+      if(q==1){
+        gridExtra::grid.arrange(grobs = plots)
+      }else{
+        gridExtra::grid.arrange(grobs = plots, ncol = 2)
+      }
+    }
+  }else{
+    mf <- stats::model.frame(object)
+    time <- stats::model.response(mf)[, 1]
+    martingale <- event - rcs
+    df <- data.frame(
+      obs = 1:length(time),
+      deviance = sign(martingale)*sqrt((-2*(martingale + event*log(event - martingale))))
+    )
+
+    ggplot(data = df, aes(x = obs, y = deviance)) +
+      geom_point() +
+      geom_abline(intercept = 0, slope = 0 , color = "blue")
+
+  }
+}
+
+
+#' ggresiduals method for coxph models
+#' @aliases ggresiduals.coxph
+#' @export
+#' @param object a fitted model object of the class coxph.
+#' @details This function produces residuals plots of Cox-Snell residuals, martingale residuals and deviance residuals.
+#' @param type type of residuals used in the plot: coxsnell (default), martingale and deviance.
+#' @param ... further arguments passed to or from other methods.
+#' @return the desired residual plot.
+#'
+ggresiduals.coxph <- function(object, type = c("coxsnell", "martingale", "deviance"), ...){
+
+  type <- tolower(type)
+  type <- match.arg(type)
+  p <- length(coef(object))
+  mf <- model.frame(object)
+  time <- model.extract(mf, "response")[,1]
+  event <- model.extract(mf, "response")[,2]
+  lp <- object$linear.predictors
+
+  martingale <- residuals(object, type = "martingale")
+  rcs <- event - martingale
+  deviance <- residuals(object, type = "deviance")
+
+
+  if(type == "coxsnell"){
+    ekm <- survfit(Surv(rcs, event)~1)
+    tb <- tibble::tibble(
+      time = ekm$time,
+      ekm = ekm$surv,
+      surv = exp(-time)
+    )
+
+    ggplot(data = tb, aes(x=ekm, y=surv)) +
+      geom_point() +
+      geom_abline(intercept = 0, slope = 1, color = "blue") +
+      labs(x = "Kaplan-Meier", y = "Cox model") +
+      xlim(0, 1) +
+      ylim(0, 1)
+
+  }else if(type == "martingale"){
+    if(p==0){
+      message("Martingale residuals plots available only for models with at least one covariate!")
+    }else{
+      labels <- names(mf)[-1]
+      tb <- mf[, labels]
       labels <- names(tb)
       q <- ncol(tb)
 
